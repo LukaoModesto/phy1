8196baefbcc90e897004db6503bd457e86a59f31
LukaoModesto <lucasoliveiractt@outlook.com>
2024-10-07 15:24:16 -0300

    chore: exercicios for and while

---
+++ exercicios-for.py
@@ -0,0 +1,104 @@
+# Exercício 1: Contagem de 1 a 10
+# Imprima os números de 1 a 10.
+
+# for c in range (1, 10+1):
+# print(c)
+
+##########################################################################
+
+# Exercício 2: Soma dos Números
+# Calcule a soma dos números de 1 a 100.
+
+# soma = 0
+
+# for c in range(1,100+1):
+#     soma +=c
+#     print(soma)
+##########################################################################
+
+# Exercício 3: Lista de Nomes
+# Crie uma lista de nomes e imprima cada um deles.
+# nomes = ["Alice", "Bob", "Carlos"]
+
+# nomes = ["Alice", "Bob", "Carlos"]
+# for nome in nomes:
+#     print(nome)
+##########################################################################
+
+# Exercício 4: Tabuada do 5
+# Imprima a tabuada do 5.
+
+# for i in range(1, 11):
+#     resultado = 5 * i
+#     print(f"5 x {i} = {resultado}")
+
+
+##########################################################################
+
+# Exercício 5: Números Pares
+# Imprima todos os números pares de 1 a 20.
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+##########################################################################
+
+# Exercício 6: Quadrados dos Números
+# Imprima os quadrados dos números de 1 a 10.
+
+# for i in range(1, 11):
+#     quadrado = i ** 2
+#     print(f"O quadrado de {i} é {quadrado}")
+
+##########################################################################
+
+# Exercício 7: Contagem Regressiva
+# Imprima uma contagem regressiva de 10 a 1.
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+##########################################################################
+
+# Exercício 8: Fatorial de um Número
+# Calcule e imprima o fatorial do número 5.
+# 5!=5×4×3×2×1=120
+# 5!=1×2×3×4×5=120
+
+# numero = 5
+# fatorial = 1
+
+# for i in range(1, numero + 1):
+#     fatorial *= i
+
+# print(f"O fatorial de {numero} é {fatorial}")
+
+
+##########################################################################
+
+# Exercício 9: Números Ímpares
+# Imprima todos os números ímpares de 1 a 20.
+
+# for i in range(1, 21):
+#     if i % 2 != 0: 
+#         print(i)
+
+##########################################################################
+
+# Exercício 10: Contar Vogais
+# crie duas variaveis pra realizar o exercício
+# string = "Hello, World!"
+# vogais = "aeiouAEIOU"
+# Conte e imprima o número de vogais em uma string.
+
+# string = "Hello, World!"
+# vogais = "aeiouAEIOU"
+
+# contador = 0
+
+# for char in string:
+#     if char in vogais:
+#         contador += 1
+
+# print(f"O número de vogais na string é: {contador}")
---
+++ exercicios-while.py
@@ -0,0 +1,127 @@
+# Exercício 1: Contagem de 1 a 10
+# Imprima os números de 1 a 10.
+
+# for i in range(1, 11):
+#     print(i)
+
+#########################################################################
+
+# Exercício 2: Soma dos Números
+# Calcule a soma dos números de 1 a 100.
+
+
+# soma = 0
+# numero = 1
+
+# while numero <= 100:
+#     soma += numero
+#     numero += 1
+
+# print(f"A soma dos números de 1 a 100 é: {soma}")
+
+
+#########################################################################
+
+# Exercício 3: Tabuada do 5
+# Imprima a tabuada do 5.
+# Tabuada do 5 usando while
+
+# i = 1
+# while i <= 10:
+#     resultado = 5 * i
+#     print(f"5 x {i} = {resultado}")
+#     i += 1
+
+
+#########################################################################
+
+# Exercício 4: Números Pares
+# Imprima todos os números pares de 1 a 20.
+
+# i = 1
+# while i <= 20:
+#     if i % 2 == 0:  # Verifica se o número é par
+#         print(i)
+#     i += 1
+
+#########################################################################
+
+# Exercício 5: Quadrados dos Números
+# Imprima os quadrados dos números de 1 a 10.
+
+# for i in range(1, 11):
+#     quadrado = i ** 2
+#     print(f"O quadrado de {i} é {quadrado}")
+
+#########################################################################
+
+# Exercício 6: Contagem Regressiva
+# Imprima uma contagem regressiva de 10 a 1.
+
+
+# i = 10
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+
+#########################################################################
+
+# Exercício 7: Fatorial de um Número
+# Calcule e imprima o fatorial do número 5.
+
+# numero = 5
+# fatorial = 1
+# i = 1
+
+# while i <= numero:
+#     fatorial *= i
+#     i += 1
+
+# print(f"O fatorial de {numero} é {fatorial}")
+
+
+#########################################################################
+
+# Exercício 8: Números Ímpares
+# Imprima todos os números ímpares de 1 a 20.
+
+# i = 1
+# while i <= 20:
+#     if i % 2 != 0:  # Verifica se o número é ímpar
+#         print(i)
+#     i += 1
+
+
+#########################################################################
+
+# Exercício 9: Contar Vogais
+# Conte e imprima o número de vogais em uma string.
+# string = "Hello, World!"
+# vogais = "aeiouAEIOU"
+
+string = "Hello, World!"
+vogais = "aeiouAEIOU"
+
+
+contador = 0
+i = 0
+
+
+while i < len(string):
+    if string[i] in vogais:
+        contador += 1
+    i += 1
+
+# Imprimir o resultado
+print(f"O número de vogais na string é: {contador}")
+
+
+
+#########################################################################
+
+# Exercício 10: Gerenciador de Lista de Compras
+# Peça ao usuário para adicionar itens à sua lista de compras até que ele digite "sair".
+# Inicializa a lista de compras vazia 
+# Loop para adicionar itens
+# Exibe a lista de compras com o loop FOR
